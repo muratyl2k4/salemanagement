7d51d612c52de9f14d83102ece86922d714c28c6
Murat Yıldırım <mybusinnes.21@gmail.com>
2023-01-29 00:47:23 +0300

    some settings

--- arayuz.py
+++ arayuz.py
@@ -25,27 +25,43 @@ class App(tk.Tk):
 class Anasayfa(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        
+
         main_canvas = tk.Canvas(self , width=SCREEN_WIDTH , height=SCREEN_HEIGTH)
         main_canvas.pack()
 
         frame = tk.Frame(self , bg='white')
         frame.place(relheight=0.70 , relwidth=0.1 ,relx=0.005 , rely= 0.25)
-
-        ##BUTTON SETTINGS
+        customer_frame = tk.Frame(self , bg='white')
+        customer_frame.place(relheight=0.70 , relwidth=0.80 ,relx=0.15 , rely=0.25 )
+        label = tk.Label(customer_frame , text='' , bg=None  , fg='black')
+        label.pack(padx=10 , pady=10 , side='top')
+        liste = ['musteri1' , 'musteri2']
         def binder(button):
             button.pack(padx=1, pady=2 , side='top')
-            button.bind('<Button-1>', lambda x=button: print(button))
+           
+            button.bind('<Button-1>', lambda x=button : customer_informations(button.cget('text')))
             button.bind('<Enter>', lambda x=button : button.config(bg='#696969', fg='white'))
             button.bind('<Leave>', lambda x=button : button.config(bg='#C5C5C5', fg='black'))
+        def musteri_ekle(musteri):
+            
+            liste.append(musteri)
+            for widget in frame.winfo_children():
+                widget.destroy()
+            for i in liste:    
+                binder(tk.Label(frame, text=i, width=16,bg='#C5C5C5' ,  relief='groove'))
+        
+        musteri_ekle_button = tk.Button(customer_frame , text='musteri ekle' , command= lambda : musteri_ekle(musteri_ekle_button.cget('text')))
+        musteri_ekle_button.pack(padx=1 , pady=1 , side='right')
+        
+        def customer_informations(customer):
+            label.config(text=customer)
+            frame.mainloop()
+            frame.place(relheight=0.70 , relwidth=0.1 ,relx=0.005 , rely= 0.25)
+            print(liste)
 
-        liste = ['musteri1' , 'musteri2']
+        ##BUTTON SETTINGS
         for i in liste:    
-            binder(tk.Label(frame, text=i, width=16,bg='#C5C5C5' ,  relief='groove'))
-        
-                
-        
-        
+                binder(tk.Label(frame, text=i, width=16,bg='#C5C5C5' ,  relief='groove'))
 
         tk.Button(self, text="Return to 1",
                   command=lambda: master.switch_frame(PageOne)).pack()
